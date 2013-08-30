108f69a979d9fddcc1e9e3f9b4df777f1e80dcf5
Oliver Meister <meistero@in.tum.de>
2013-08-30 04:27:43 -0700

    Updated header with a bit of documentation

--- PyOP2/samoa.h
+++ PyOP2/samoa.h
@@ -2,5 +2,21 @@
 // Copyright (C) 2010 Oliver Meister, Kaveh Rahnema
 // This program is licensed under the GPL, for details see the file LICENSE
 
-//> Kernel execution wrapper interface
-extern "C" void samoa_run_c_kernel(void (*)(const int cell_index, const int edge_indices[3], const int vertex_indices[3], const double coords[6], char* refinement));
+/** \brief Executes a kernel on a triangular mesh in Sierpinski order
+ * The following order is chosen for local edge and vertex numbering:
+ * 2
+ * |\
+ * | \
+ * 0  1
+ * |   \
+ * |    \
+ * 1--2--0
+ *
+ *
+ * \param cell_index        index of the cell
+ * \param edge_indices      map from local to global edge indices
+ * \param vertex_indices    map from local to global vbertex indices
+ * \param coords            vertex coordinates in local order
+ *
+ */
+extern "C" void samoa_run_c_kernel(void (*)(const int cell_index, const int edge_indices[3], const int vertex_indices[3], const double coords[3][2], char* refinement));
