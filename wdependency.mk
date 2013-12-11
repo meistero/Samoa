fb5ca542d19eedf7beba21bb634529bab4bc0ce1
Oliver Meister <meistero@in.tum.de>
2013-12-11 19:15:39 +0100

    Updated Windows Makefile - probably won't work yet

--- wdependency.mk
+++ wdependency.mk
@@ -1,59 +1,75 @@
-# DO NOT EDIT --- auto-generated file
-SFC_checkpointing.obj: SFC_data_types.obj SFC_node_traversal.obj
-SFC_containers.obj: SFC_data_types.obj
-SFC_data_types.obj: Tools_log.obj Heat_Equation/Heat_Eq_data_types.obj Darcy/Darcy_data_types.obj Tests/Tests_data_types.obj
-SFC_fine_grid.obj: SFC_data_types.obj Tools_stack_base.obj
-SFC_data_types.obj: SFC_data_types.obj
-SFC_edge_traversal.obj: SFC_data_types.obj SFC_fine_grid.obj SFC_containers.obj
-SFC_node_traversal.obj: SFC_edge_traversal.obj
-SFC_traversal.obj: SFC_node_traversal.obj Heat_Equation/Heat_Eq.obj Darcy/Darcy.obj Tests/Tests.obj
-SFC_main.obj: M_kracken.obj SFC_traversal.obj
-Tools_local_function_space_base.obj: SFC_data_types.obj
-Tools_noise.obj: SFC_data_types.obj
-Tools_stack_base.obj: SFC_data_types.obj
+SFC_data_types.o: Tools_log.o Heat_Equation/Heat_Eq_data_types.o Darcy/Darcy_data_types.o SWE/SWE_data_types.o Tests/Tests_data_types.o Generic/Generic_data_types.o #NUMA/NUMA_data_types.o
+SFC_edge_traversal.o: SFC_data_types.o
+SFC_node_traversal.o: SFC_edge_traversal.o
+SFC_traversal.o: SFC_node_traversal.o Heat_Equation/Heat_Eq.o Darcy/Darcy.o Tests/Tests.o SWE/SWE.o Generic/Generic.o #NUMA/NUMA.o
+SFC_main.o: M_kracken.o SFC_traversal.o
+Tools_local_function_space_base.o: SFC_data_types.o
+Tools_noise.o: SFC_data_types.o
+Tools_stack_base.o: SFC_data_types.o
 
-Tests/Tests.obj: Tests/Tests_data_types.obj Tests/Tests_basis_functions.obj Tests/Tests_initialize.obj Tests/Tests_node_dummy_traversal.obj Tests/Tests_flops_traversal.obj Tests/Tests_memory_traversal.obj Tests/Tests_consistency_traversal.obj Samoa/Samoa.obj
-Tests/Tests_initialize.obj: SFC_node_traversal.obj
-Tests/Tests_node_dummy_traversal.obj: SFC_node_traversal.obj
-Tests/Tests_consistency_traversal.obj: SFC_node_traversal.obj
-Tests/Tests_flops_traversal.obj: SFC_node_traversal.obj
-Tests/Tests_memory_traversal.obj: SFC_node_traversal.obj
-Tests/Tests_basis_functions.obj: SFC_data_types.obj
+Conformity/Conformity.o: SFC_edge_traversal.o
 
-Darcy/Darcy.obj: Darcy/Darcy_data_types.obj Darcy/Darcy_initialize.obj Darcy/Darcy_node_dummy.obj Darcy/Darcy_output.obj Darcy/Darcy_xml_output.obj Darcy/Darcy_grad_p.obj Darcy/Darcy_laplace_jacobi.obj Darcy/Darcy_laplace_cg.obj Darcy/Darcy_transport_eq.obj Darcy/Darcy_permeability.obj Darcy/Darcy_adapt.obj Samoa/Samoa.obj
-Darcy/Darcy_adapt.obj: SFC_node_traversal.obj Darcy/Darcy_local_function_spaces.obj Samoa/Samoa_quadrature_rules.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_node_dummy.obj: SFC_node_traversal.obj
-Darcy/Darcy_grad_p.obj: SFC_node_traversal.obj Darcy/Darcy_local_function_spaces.obj Samoa/Samoa_quadrature_rules.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_initialize.obj: Tools_noise.obj SFC_node_traversal.obj Samoa/Samoa_quadrature_rules.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_laplace_cg.obj: SFC_node_traversal.obj Samoa/Samoa_quadrature_rules.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_laplace_jacobi.obj: SFC_node_traversal.obj Samoa/Samoa_quadrature_rules.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_local_function_spaces.obj: Tools_local_function_space_base.obj SFC_data_types.obj
-Darcy/Darcy_output.obj: SFC_node_traversal.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_xml_output.obj: SFC_node_traversal.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj LIB_VTK_IO.obj
-Darcy/Darcy_transport_eq.obj: SFC_node_traversal.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_permeability.obj: SFC_node_traversal.obj Darcy/Darcy_basis.obj Samoa/Samoa.obj
-Darcy/Darcy_basis.obj: SFC_data_types.obj Darcy/Darcy_local_function_spaces.obj Samoa/Samoa.obj
+geoclaw/c_bind_riemannsolvers.o: geoclaw/riemannsolvers.o
 
-Heat_Equation/Heat_Eq.obj: Heat_Equation/Heat_Eq_data_types.obj Heat_Equation/Heat_Eq_basis.obj Heat_Equation/Heat_Eq_initialize.obj Heat_Equation/Heat_Eq_output.obj Heat_Equation/Heat_Eq_xml_output.obj Heat_Equation/Heat_Eq_euler_timestep.obj Heat_Equation/Heat_Eq_midpoint_timestep.obj Heat_Equation/Heat_Eq_heun_timestep.obj Heat_Equation/Heat_Eq_adapt.obj Samoa/Samoa.obj
-Heat_Equation/Heat_Eq_adapt.obj: SFC_node_traversal.obj Heat_Equation/Heat_Eq_basis.obj Samoa/Samoa.obj
-Heat_Equation/Heat_Eq_euler_timestep.obj: SFC_node_traversal.obj Heat_Equation/Heat_Eq_basis.obj Samoa/Samoa.obj
-Heat_Equation/Heat_Eq_heun_timestep.obj: Heat_Equation/Heat_Eq_euler_timestep.obj
-Heat_Equation/Heat_Eq_initialize.obj: Tools_noise.obj SFC_node_traversal.obj Heat_Equation/Heat_Eq_basis.obj Samoa/Samoa.obj Heat_Equation/Heat_Eq_local_function_spaces.obj
-Heat_Equation/Heat_Eq_local_function_spaces.obj: Tools_local_function_space_base.obj SFC_data_types.obj
-Heat_Equation/Heat_Eq_midpoint_timestep.obj: Heat_Equation/Heat_Eq_euler_timestep.obj
-Heat_Equation/Heat_Eq_output.obj: SFC_node_traversal.obj Heat_Equation/Heat_Eq_basis.obj Heat_Equation/Heat_Eq_local_function_spaces.obj Samoa/Samoa.obj
-Heat_Equation/Heat_Eq_xml_output.obj: SFC_node_traversal.obj Heat_Equation/Heat_Eq_basis.obj Heat_Equation/Heat_Eq_local_function_spaces.obj Samoa/Samoa.obj LIB_VTK_IO.obj
-Heat_Equation/Heat_Eq_basis.obj: SFC_data_types.obj Heat_Equation/Heat_Eq_local_function_spaces.obj Samoa/Samoa.obj
+Tests/Tests.o: Tests/Tests_data_types.o Tests/Tests_basis_functions.o Tests/Tests_initialize.o Tests/Tests_node_dummy_traversal.o Tests/Tests_flops_traversal.o Tests/Tests_memory_traversal.o Tests/Tests_consistency_traversal.o Samoa/Samoa.o
+Tests/Tests_basis_functions.o: SFC_data_types.o
+Tests/Tests_initialize.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o
+Tests/Tests_node_dummy_traversal.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o
+Tests/Tests_consistency_traversal.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o
+Tests/Tests_flops_traversal.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o
+Tests/Tests_memory_traversal.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o
 
-Shallow_Water/Shallow_Water.obj: Shallow_Water/Shallow_Water_data_types.obj Shallow_Water/Shallow_Water_basis.obj Shallow_Water/Shallow_Water_initialize.obj Shallow_Water/Shallow_Water_xml_output.obj Shallow_Water/Shallow_Water_euler_timestep.obj Shallow_Water/Shallow_Water_adapt.obj Samoa/Samoa.obj
-Shallow_Water/Shallow_Water_adapt.obj: SFC_node_traversal.obj Shallow_Water/Shallow_Water_basis.obj Samoa/Samoa.obj
-Shallow_Water/Shallow_Water_euler_timestep.obj: SFC_node_traversal.obj Shallow_Water/Shallow_Water_basis.obj Samoa/Samoa.obj
-Shallow_Water/Shallow_Water_initialize.obj: Tools_noise.obj SFC_node_traversal.obj Shallow_Water/Shallow_Water_basis.obj Samoa/Samoa.obj Shallow_Water/Shallow_Water_local_function_spaces.obj
-Shallow_Water/Shallow_Water_local_function_spaces.obj: Tools_local_function_space_base.obj SFC_data_types.obj
-Shallow_Water/Shallow_Water_xml_output.obj: SFC_node_traversal.obj Shallow_Water/Shallow_Water_basis.obj Shallow_Water/Shallow_Water_local_function_spaces.obj Samoa/Samoa.obj LIB_VTK_IO.obj
-Shallow_Water/Shallow_Water_basis.obj: SFC_data_types.obj Shallow_Water/Shallow_Water_local_function_spaces.obj Samoa/Samoa.obj
+Darcy/Darcy.o: Darcy/Darcy_data_types.o Solver/LinearSolver.o Darcy/Darcy_initialize.o Darcy/Darcy_node_dummy.o Darcy/Darcy_output.o Darcy/Darcy_xml_output.o Darcy/Darcy_grad_p.o Darcy/Darcy_laplace_jacobi.o Darcy/Darcy_laplace_cg.o Darcy/Darcy_transport_eq.o Darcy/Darcy_permeability.o Darcy/Darcy_adapt.o Samoa/Samoa.o
+Darcy/Darcy_local_function_spaces.o: SFC_data_types.o Tools_grid_variable.f90 Tools_local_function_space.f90
+Darcy/Darcy_basis.o: SFC_data_types.o Darcy/Darcy_local_function_spaces.o Samoa/Samoa.o
+Darcy/Darcy_adapt.o: SFC_generic_adaptive_traversal.f90 Conformity/Conformity.o Darcy/Darcy_local_function_spaces.o Darcy/Darcy_basis.o Samoa/Samoa.o Darcy/Darcy_initialize.o
+Darcy/Darcy_node_dummy.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o
+Darcy/Darcy_grad_p.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Darcy/Darcy_local_function_spaces.o Darcy/Darcy_basis.o Samoa/Samoa.o
+Darcy/Darcy_initialize.o: SFC_generic_traversal_ringbuffer.f90 Tools_noise.o SFC_node_traversal.o Darcy/Darcy_basis.o Samoa/Samoa.o
+Darcy/Darcy_laplace_cg.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Darcy/Darcy_basis.o Samoa/Samoa.o
+Darcy/Darcy_laplace_jacobi.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Darcy/Darcy_basis.o Samoa/Samoa.o
+Darcy/Darcy_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Darcy/Darcy_basis.o Samoa/Samoa.o LIB_VTK_IO.o
+Darcy/Darcy_xml_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Darcy/Darcy_basis.o Samoa/Samoa.o LIB_VTK_IO.o
+Darcy/Darcy_transport_eq.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Darcy/Darcy_basis.o Samoa/Samoa.o
+Darcy/Darcy_permeability.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Darcy/Darcy_basis.o Samoa/Samoa.o
 
-Samoa/Samoa.obj: SFC_data_types.obj Tools_local_function_space_base.obj Samoa/Tools_quadrature_rule_base.obj
-Samoa/Samoa_quadrature_rules.obj: SFC_data_types.obj Samoa/Samoa.obj Darcy/Darcy_local_function_spaces.obj Heat_Equation/Heat_Eq_local_function_spaces.obj Samoa/Samoa.obj
-Samoa/Tools_quadrature_rule_base.obj: SFC_data_types.obj
-Samoa/Tools_quadrature_rule.obj: SFC_data_types.obj Samoa/Tools_quadrature_rule_base.obj
+Heat_Equation/Heat_Eq.o: Heat_Equation/Heat_Eq_data_types.o Heat_Equation/Heat_Eq_basis.o Heat_Equation/Heat_Eq_initialize.o Heat_Equation/Heat_Eq_output.o Heat_Equation/Heat_Eq_xml_output.o Heat_Equation/Heat_Eq_euler_timestep.o Heat_Equation/Heat_Eq_midpoint_timestep.o Heat_Equation/Heat_Eq_heun_timestep.o Heat_Equation/Heat_Eq_adapt.o Samoa/Samoa.o
+Heat_Equation/Heat_Eq_local_function_spaces.o: SFC_data_types.o
+Heat_Equation/Heat_Eq_basis.o: SFC_data_types.o Heat_Equation/Heat_Eq_local_function_spaces.o Samoa/Samoa.o
+Heat_Equation/Heat_Eq_adapt.o: SFC_generic_adaptive_traversal.f90 Conformity/Conformity.o Heat_Equation/Heat_Eq_basis.o Samoa/Samoa.o
+Heat_Equation/Heat_Eq_euler_timestep.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Heat_Equation/Heat_Eq_basis.o Samoa/Samoa.o
+Heat_Equation/Heat_Eq_heun_timestep.o: SFC_generic_traversal_ringbuffer.f90 Heat_Equation/Heat_Eq_euler_timestep.o
+Heat_Equation/Heat_Eq_initialize.o: SFC_generic_traversal_ringbuffer.f90 Tools_noise.o SFC_node_traversal.o Heat_Equation/Heat_Eq_basis.o Samoa/Samoa.o Heat_Equation/Heat_Eq_local_function_spaces.o
+Heat_Equation/Heat_Eq_midpoint_timestep.o: SFC_generic_traversal_ringbuffer.f90 Heat_Equation/Heat_Eq_euler_timestep.o
+Heat_Equation/Heat_Eq_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Heat_Equation/Heat_Eq_basis.o Heat_Equation/Heat_Eq_local_function_spaces.o Samoa/Samoa.o LIB_VTK_IO.o
+Heat_Equation/Heat_Eq_xml_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o Heat_Equation/Heat_Eq_basis.o Heat_Equation/Heat_Eq_local_function_spaces.o Samoa/Samoa.o LIB_VTK_IO.o
+
+SWE/SWE.o: SWE/SWE_data_types.o SWE/SWE_displace.o SWE/SWE_basis.o SWE/SWE_initialize.o SWE/SWE_output.o SWE/SWE_xml_output.o SWE/SWE_euler_timestep.o SWE/SWE_adapt.o Samoa/Samoa.o
+SWE/SWE_local_function_spaces.o: SFC_data_types.o
+SWE/SWE_basis.o: SFC_data_types.o SWE/SWE_local_function_spaces.o Samoa/Samoa.o
+SWE/SWE_adapt.o: SFC_generic_adaptive_traversal.f90 Conformity/Conformity.o SWE/SWE_basis.o Samoa/Samoa.o SWE/SWE_euler_timestep.o SWE/SWE_initialize.o
+SWE/SWE_euler_timestep.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o SWE/SWE_basis.o Samoa/Samoa.o geoclaw/c_bind_riemannsolvers.o
+SWE/SWE_initialize.o: SFC_generic_traversal_ringbuffer.f90 SWE/SWE_euler_timestep.o Tools_noise.o SFC_node_traversal.o SWE/SWE_basis.o Samoa/Samoa.o SWE/SWE_local_function_spaces.o
+SWE/SWE_displace.o: SWE/SWE_initialize.o SFC_generic_traversal_ringbuffer.f90 SWE/SWE_euler_timestep.o Tools_noise.o SFC_node_traversal.o SWE/SWE_basis.o Samoa/Samoa.o SWE/SWE_local_function_spaces.o
+SWE/SWE_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o SWE/SWE_basis.o SWE/SWE_local_function_spaces.o SWE/SWE_euler_timestep.o Samoa/Samoa.o LIB_VTK_IO.o
+SWE/SWE_xml_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o SWE/SWE_basis.o SWE/SWE_local_function_spaces.o SWE/SWE_euler_timestep.o Samoa/Samoa.o LIB_VTK_IO.o
+
+#NUMA/NUMA.o: NUMA/NUMA_data_types.o NUMA/NUMA_basis.o NUMA/NUMA_initialize.o NUMA/NUMA_output.o NUMA/NUMA_xml_output.o NUMA/NUMA_euler_timestep.o NUMA/NUMA_adapt.o Samoa/Samoa.o
+#NUMA/NUMA_local_function_spaces.o: SFC_data_types.o
+#NUMA/NUMA_basis.o: SFC_data_types.o NUMA/NUMA_local_function_spaces.o Samoa/Samoa.o
+#NUMA/NUMA_adapt.o: SFC_generic_adaptive_traversal.f90 Conformity/Conformity.o NUMA/NUMA_basis.o Samoa/Samoa.o NUMA/NUMA_euler_timestep.o NUMA/NUMA_initialize.o
+#NUMA/NUMA_euler_timestep.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o NUMA/NUMA_basis.o NUMA/NUMA_constants.o Samoa/Samoa.o geoclaw/c_bind_riemannsolvers.o
+#NUMA/NUMA_initialize.o: SFC_generic_traversal_ringbuffer.f90 NUMA/NUMA_euler_timestep.o Tools_noise.o SFC_node_traversal.o NUMA/NUMA_basis.o Samoa/Samoa.o NUMA/NUMA_local_function_spaces.o
+#NUMA/NUMA_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o NUMA/NUMA_basis.o NUMA/NUMA_local_function_spaces.o NUMA/NUMA_euler_timestep.o Samoa/Samoa.o LIB_VTK_IO.o
+#NUMA/NUMA_xml_output.o: SFC_generic_traversal_ringbuffer.f90 SFC_node_traversal.o NUMA/NUMA_basis.o NUMA/NUMA_local_function_spaces.o NUMA/NUMA_euler_timestep.o Samoa/Samoa.o LIB_VTK_IO.o
+#NUMA/NUMA_constants.o: NUMA/NUMA_data_types.o
+
+Generic/Generic.o: Generic/Generic_data_types.o  SFC_node_traversal.o Generic/Generic_initialize.o Generic/Generic_template.o Generic/Generic_adapt_template.o
+Generic/Generic_initialize.o: SFC_generic_traversal_ringbuffer.f90 SFC_edge_traversal.o Samoa/Samoa.o
+Generic/Generic_template.o: SFC_generic_traversal_ringbuffer.f90 SFC_edge_traversal.o Samoa/Samoa.o
+Generic/Generic_adapt_template.o: SFC_generic_adaptive_traversal.f90 Conformity/Conformity.o Samoa/Samoa.o
+
+Samoa/Samoa.o: SFC_data_types.o Samoa/Tools_quadrature_rule_base.o
+Samoa/Tools_quadrature_rule_base.o: SFC_data_types.o
+
+Solver/LinearSolver.o: SFC_edge_traversal.o
--- wmake.bat
+++ wmake.bat
@@ -1,11 +1,4 @@
 @echo off
 cls
 
-make -j -f WMakefile
-
-if %errorlevel% neq 0 (
-	echo "Aborting script: make returned an error."
-	exit
-)
-
-wrun.bat %*
+nmake -f WMakefile %@
