882299ba782bfd8d31ca6893dd247c6ed8812198
Oliver Meister <meistero@in.tum.de>
2014-10-20 22:19:58 +0200

    Removed divergence correction, it is incorporated into the upwind solver now. Also simplified advection step

--- src/Darcy/Darcy_transport_eq.f90
+++ src/Darcy/Darcy_transport_eq.f90
@@ -18,7 +18,6 @@
 		type(darcy_gv_saturation)				:: gv_saturation
 		type(darcy_gv_volume)					:: gv_volume
 		type(darcy_gv_flux)						:: gv_flux
-		type(darcy_gv_d)						:: gv_diverg
 
 #		define _GT_NAME							t_darcy_transport_eq_traversal
 
@@ -31,12 +30,10 @@
 #		define _GT_PRE_TRAVERSAL_GRID_OP		pre_traversal_grid_op
 #		define _GT_POST_TRAVERSAL_GRID_OP		post_traversal_grid_op
 
-#		define _GT_INNER_ELEMENT_OP				inner_element_op
 #		define _GT_ELEMENT_OP					element_op
 
 #		define _GT_NODE_FIRST_TOUCH_OP		    node_first_touch_op
 #		define _GT_NODE_LAST_TOUCH_OP		    node_last_touch_op
-#		define _GT_INNER_NODE_LAST_TOUCH_OP		inner_node_last_touch_op
 
 #		define _GT_NODE_MERGE_OP		        node_merge_op
 
@@ -93,27 +90,6 @@
 
 		!element
 
-		subroutine inner_element_op(traversal, section, element)
- 			type(t_darcy_transport_eq_traversal), intent(inout)	    :: traversal
- 			type(t_grid_section), intent(inout)						:: section
-			type(t_element_base), intent(inout), target				:: element
-
-			real(kind = GRID_SR), dimension(2, _DARCY_U_SIZE)		:: u
-			real(kind = GRID_SR), dimension(_DARCY_FLOW_SIZE)		:: saturation
-			real(kind = GRID_SR), dimension(_DARCY_FLOW_SIZE)		:: flux, diverg
-			real(kind = GRID_SR), dimension(_DARCY_FLOW_SIZE)		:: volume
-
-			call gv_u%read_from_element(element, u)
-			call gv_saturation%read(element, saturation)
-
-			!call volume operator
-			call compute_fluxes(element, u, saturation, volume, flux, diverg)
-
-			call gv_volume%add(element, volume)
-			call gv_flux%add(element, flux)
-			call gv_diverg%add(element, diverg)
-		end subroutine
-
 		subroutine element_op(traversal, section, element)
  			type(t_darcy_transport_eq_traversal), intent(inout)	    :: traversal
  			type(t_grid_section), intent(inout)						:: section
@@ -121,18 +97,17 @@
 
 			real(kind = GRID_SR), dimension(2, _DARCY_U_SIZE)		:: u
 			real(kind = GRID_SR), dimension(_DARCY_FLOW_SIZE)		:: saturation
-			real(kind = GRID_SR), dimension(_DARCY_FLOW_SIZE)		:: flux, diverg
+			real(kind = GRID_SR), dimension(_DARCY_FLOW_SIZE)		:: flux
 			real(kind = GRID_SR), dimension(_DARCY_FLOW_SIZE)		:: volume
 
 			call gv_u%read_from_element(element, u)
 			call gv_saturation%read(element, saturation)
 
 			!call volume operator
-			call compute_fluxes(element, u, saturation, volume, flux, diverg)
+			call compute_fluxes(element, u, saturation, volume, flux)
 
 			call gv_volume%add(element, volume)
 			call gv_flux%add(element, flux)
-			call gv_diverg%add(element, diverg)
 		end subroutine
 
 		!first touches
@@ -142,103 +117,46 @@
  			type(t_grid_section), intent(in)							:: section
 			type(t_node_data), intent(inout)				:: node
 
-			call pre_dof_op(node%data_temp%flux, node%data_temp%volume, node%data_pers%d)
+			call pre_dof_op(node%data_temp%flux, node%data_temp%volume)
 		end subroutine
 
 		!last touches
-
-		elemental subroutine inner_node_last_touch_op(traversal, section, node)
- 			type(t_darcy_transport_eq_traversal), intent(in)	    :: traversal
- 			type(t_grid_section), intent(in)							:: section
-			type(t_node_data), intent(inout)				:: node
-
-            !do saturation update without divergence correction
-            call inner_post_dof_op(section%r_dt, node%data_pers%saturation, node%data_temp%flux, node%data_temp%volume)
-		end subroutine
 
 		elemental subroutine node_last_touch_op(traversal, section, node)
  			type(t_darcy_transport_eq_traversal), intent(in)	    :: traversal
  			type(t_grid_section), intent(in)							:: section
 			type(t_node_data), intent(inout)				:: node
 
-            if (any(node%data_temp%is_dirichlet_boundary)) then
-                !do a saturation update with divergence correction
-                call post_dof_op(section%r_dt, node%data_pers%saturation, node%data_temp%flux, node%data_temp%volume, node%data_pers%d)
-            else
-                !do a saturation update without divergence correction
-                call inner_post_dof_op(section%r_dt, node%data_pers%saturation, node%data_temp%flux, node%data_temp%volume)
-            end if
+            call post_dof_op(section%r_dt, node%data_pers%saturation, node%data_temp%flux, node%data_temp%volume)
 		end subroutine
 
 		!*******************************
 		!Volume and DoF operators
 		!*******************************
 
-		elemental subroutine pre_dof_op(flux, volume, diverg)
+		elemental subroutine pre_dof_op(flux, volume)
  			real (kind = GRID_SR), intent(out)		:: flux
 			real (kind = GRID_SR), intent(out)		:: volume
-			real (kind = GRID_SR), intent(out)		:: diverg
 
 			flux = 0.0_GRID_SR
 			volume = 0.0_GRID_SR
-			diverg = 0.0_SR
 		end subroutine
 
-		subroutine compute_inner_fluxes(element, u_in, saturation, volume, flux)
-			type(t_element_base), intent(inout)									:: element
-			real (kind = GRID_SR), dimension(:, :), intent(in)					:: u_in
-			real (kind = GRID_SR), dimension(:), intent(in)						:: saturation
-			real (kind = GRID_SR), dimension(:), intent(out)					:: volume
-			real (kind = GRID_SR), dimension(:), intent(out)					:: flux
-
-			real (kind = GRID_SR)									:: u(2), n(2), r_dual_edge_length
-            real (kind = GRID_SR)								    :: u_dot_n, lambda_w(3)
-            real (kind = GRID_SR)               				    :: pos_prod(2), radius
-            integer (kind = GRID_SI)							    :: i
-
-			volume(:) = [0.25_GRID_SR, 0.50_GRID_SR, 0.25_GRID_SR] * cfg%scaling * cfg%scaling * element%cell%data_pers%porosity * element%cell%geometry%get_volume()
-
-            lambda_w(:) = (saturation(:) * saturation(:)) / cfg%r_nu_w
-
-			u(:) = samoa_world_to_barycentric_normal(element%transform_data, u_in(:, 1))
-			u = u * sqrt(dot_product(u_in(:, 1), u_in(:, 1)) / dot_product(u, u))
-			r_dual_edge_length = 0.5_GRID_SR * cfg%scaling * element%cell%geometry%get_leg_size()
-
-			!compute an upwind flux
-
-#           if defined(_UPWIND_FLUX)
-                flux(1) = (lambda_w(2) * max(u(1), 0.0_GRID_SR) + lambda_w(1) * min(u(1), 0.0_GRID_SR))
-                flux(3) = (lambda_w(2) * max(u(2), 0.0_GRID_SR) + lambda_w(3) * min(u(2), 0.0_GRID_SR))
-                flux(2) = -flux(1) - flux(3)
-#           elif defined(_FWAVE_FLUX)
-                flux(1) = (lambda_w(2) - lambda_w(1)) * max(u(1), 0.0_GRID_SR)
-                flux(3) = (lambda_w(2) - lambda_w(3)) * max(u(2), 0.0_GRID_SR)
-                flux(2) = (lambda_w(1) - lambda_w(2)) * max(-u(1), 0.0_GRID_SR) + (lambda_w(3) - lambda_w(2)) * max(-u(2), 0.0_GRID_SR)
-#           endif
-
-            flux(:) = r_dual_edge_length * flux(:)
-		end subroutine
-
-        subroutine compute_fluxes(element, u_in, saturation, volume, flux, diverg)
+        subroutine compute_fluxes(element, u_in, saturation, volume, flux)
 			type(t_element_base), intent(inout)									:: element
 			real (kind = GRID_SR), dimension(:, :), intent(in)					:: u_in
 			real (kind = GRID_SR), dimension(:), intent(in)						:: saturation
 			real (kind = GRID_SR), dimension(:), intent(out)	    :: volume
 			real (kind = GRID_SR), dimension(:), intent(out)	    :: flux
-			real (kind = GRID_SR), dimension(:), intent(out)	    :: diverg
 
-			real (kind = GRID_SR)									:: u(2), n(2), r_dual_edge_length
-            real (kind = GRID_SR)								    :: u_dot_n, lambda_w(3), lambda_n(3)
-            real (kind = GRID_SR)               				    :: pos_prod(2), radius
-            integer (kind = GRID_SI)							    :: i
+			real (kind = GRID_SR)									:: u(2), r_dual_edge_length
+            real (kind = GRID_SR)								    :: lambda_w(3)
 
 			volume(:) = [0.25_GRID_SR, 0.50_GRID_SR, 0.25_GRID_SR] * cfg%scaling * cfg%scaling * element%cell%data_pers%porosity * element%cell%geometry%get_volume()
 
             lambda_w(:) = (saturation(:) * saturation(:)) / cfg%r_nu_w
-            lambda_n(:) = (1.0_SR - saturation(:)) * (1.0_SR - saturation(:)) / cfg%r_nu_n
 
-			!compute an upwind flux
-			!also, accumulate the divergence on boundary dual cells
+			!compute fluxes
 
 			u(:) = samoa_world_to_barycentric_normal(element%transform_data, u_in(:, 1))
 			u = u / (element%transform_data%custom_data%scaling * sqrt(abs(element%transform_data%plotter_data%det_jacobian)))
@@ -247,52 +165,35 @@
 			r_dual_edge_length = 0.5_GRID_SR * cfg%scaling * element%cell%geometry%get_leg_size()
 
 #           if defined(_UPWIND_FLUX)
+                !compute flux for the equation Phi * S_t + < u, grad f(S) > = 0
                 flux(1) = (lambda_w(2) * max(u(1), 0.0_GRID_SR) + lambda_w(1) * min(u(1), 0.0_GRID_SR))
                 flux(3) = (lambda_w(2) * max(u(2), 0.0_GRID_SR) + lambda_w(3) * min(u(2), 0.0_GRID_SR))
                 flux(2) = -flux(1) - flux(3)
+
+                !Solve the source term equation Phi * S_t = -div(u) * f(S)
+                flux(:) = (flux(:) - lambda_w(:) * [u(1), -u(1) -u(2), u(2)]) * r_dual_edge_length
+
+                !together, both steps solve the conservative equation Phi * S_t + div(u * f(S)) = 0
 #           elif defined(_FWAVE_FLUX)
                 flux(1) = (lambda_w(2) - lambda_w(1)) * max(u(1), 0.0_GRID_SR)
                 flux(3) = (lambda_w(2) - lambda_w(3)) * max(u(2), 0.0_GRID_SR)
                 flux(2) = (lambda_w(1) - lambda_w(2)) * max(-u(1), 0.0_GRID_SR) + (lambda_w(3) - lambda_w(2)) * max(-u(2), 0.0_GRID_SR)
+
+                flux(:) = flux(:) * r_dual_edge_length
 #           endif
-
-			diverg(1) = dot_product([0.25_SR, 0.5_SR, 0.25_SR], lambda_w(:) + lambda_n(:)) * u(1) * r_dual_edge_length
-			diverg(3) = dot_product([0.25_SR, 0.5_SR, 0.25_SR], lambda_w(:) + lambda_n(:)) * u(2) * r_dual_edge_length
-			diverg(2) = -diverg(1) - diverg(3)
-
-            flux(:) = r_dual_edge_length * flux(:)
 
             !Careful: the FEM pressure solution implies that u = 0 on the boundary.
             !If we defined an outflow condition in the FV step, we would get a non-zero divergence.
 		end subroutine
 
-		elemental subroutine inner_post_dof_op(dt, saturation, flux, volume)
+		elemental subroutine post_dof_op(dt, saturation, flux, volume)
 			real (kind = GRID_SR), intent(in)		:: dt
 			real (kind = GRID_SR), intent(inout)	:: saturation
 			real (kind = GRID_SR), intent(in)		:: flux
 			real (kind = GRID_SR), intent(in)		:: volume
 
             if (volume > 0.0_SR) then
-                saturation = saturation + dt * flux / volume
-            end if
-
-            !assert_pure(saturation <= 1.0_SR)
-		end subroutine
-
-		elemental subroutine post_dof_op(dt, saturation, flux, volume, diverg)
-			real (kind = GRID_SR), intent(in)		:: dt
- 			real (kind = GRID_SR), intent(inout)	:: saturation
-			real (kind = GRID_SR), intent(in)		:: flux
-			real (kind = GRID_SR), intent(in)		:: volume
-			real (kind = GRID_SR), intent(in)		:: diverg
-
-            real (kind = GRID_SR)				    :: lambda_w, lambda_n
-
-            if (volume > 0.0_SR) then
-                lambda_w = (saturation * saturation) / cfg%r_nu_w
-                lambda_n = (1.0_SR - saturation) * (1.0_SR - saturation) / cfg%r_nu_n
-
-                saturation = saturation + dt * (flux - lambda_w / (lambda_w + lambda_n) * diverg) / volume
+                saturation = saturation + dt / volume * flux
             end if
 
             !assert_pure(saturation <= 1.0_SR)
