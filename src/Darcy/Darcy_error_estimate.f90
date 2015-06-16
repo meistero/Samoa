4c92b79171aeda4b8d09817a6854d46aeb56fb1c
Oliver Meister <meistero@in.tum.de>
2015-06-16 14:40:34 +0200

    Added different refinement thresholds for 2D and 3D

--- src/Darcy/Darcy_error_estimate.f90
+++ src/Darcy/Darcy_error_estimate.f90
@@ -137,11 +137,15 @@
 			integer (kind = BYTE), intent(in)			:: i_depth
 			integer (kind = BYTE), intent(out)			:: i_refinement
 
-!            real (kind = GRID_SR), parameter            :: refinement_threshold = 1.0e2_SR
-            real (kind = GRID_SR), parameter            :: refinement_threshold = 1.0e8_SR
+#           if (_DARCY_LAYERS > 0)
+!               real (kind = GRID_SR), parameter            :: refinement_threshold = 1.0e2_SR
+                real (kind = GRID_SR), parameter            :: refinement_threshold = 1.0e8_SR
+#           else
+                real (kind = GRID_SR), parameter            :: refinement_threshold = 1.0e1_SR
+#           endif
 
-            real (kind = GRID_SR)						:: r_sat_norm, r_p_norm
-            logical 									:: l_coarsen_p, l_coarsen_sat, l_refine_sat, l_relevant
+            real (kind = GRID_SR)						    :: r_sat_norm, r_p_norm
+            logical 									    :: l_coarsen_p, l_coarsen_sat, l_refine_sat, l_relevant
 
 #           if (_DARCY_LAYERS > 0)
                 real (kind = GRID_SR), intent(in)		    :: saturation(:,:)
